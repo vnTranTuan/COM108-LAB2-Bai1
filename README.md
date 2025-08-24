d6ae08a8dfe0ec3ed376573b7fa900286bdb2bc7
vnTranTuan <trtuan@outlook.com>
2025-08-24 22:00:46 +0700

    LAB2, Bài 1 Hướng dẫn và Starter Code

---
+++ README.md
@@ -0,0 +1,10 @@
+YÊU CẦU
+Xây dựng chương trình cộng / trừ (+/-) 2 số
+Input : Nhập vào từ bàn phím 2 số
+Output: Hiển thị ra màn hình tổng, hiệu của 2 số
+
+
+HƯỚNG DẪN THỰC HIỆN
+1. Khai báo các biến số A, B và các kết quả Tổng, Hiệu của 2 số
+2. Dùng lệnh scanf để nhập từ bàn phím
+3. Dùng lệnh printf để hiển thị nội dung ra màn hình
---
+++ bai1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // Chao mung ban den voi FPOLY
+    // Mon hoc COM108 - Nhap mon lap trinh
+    // LAB2 - bai1
+    printf("Hello, FPOLY!\n");
+    printf("COM108, LAB2 - bai1\n");
+
+    // Cac lenh nhap vao so A
+
+    
+
+
+    // Cac lenh nhap vao so B
+
+    
+
+
+    // Cac lenh tinh tong cac so A, va B
+
+    
+    
+    return 0;
+}
